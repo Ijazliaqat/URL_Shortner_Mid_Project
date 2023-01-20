c938c8f3410791aa8fdfb734b793ae4cbbfba5a5
Faizan <faizani1292@gmail.com>
2023-01-20 12:37:10 +0500

    #comment Changes in header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,11 +3,13 @@ import {Link} from "react-router-dom";
 
 function Header() {
   return (
+    <>
     <nav>
-      <Link class="menu_btn" to="/" style={{fontSize:'25px', color:'white', fontFamily:'Verdana'}}>Short.ly</Link>
-      <Link class="menu_btn" to="/">Home</Link> 
-      <Link class="menu_btn" to="/history">History</Link>
+      <Link className='menu_btn' to="/" style={{fontSize:'25px', color:'white', fontFamily:'Verdana'}}>Short.ly</Link>
+      <Link className='menu_btn' to="/">Home</Link> 
+      <Link className='menu_btn' to="/history">History</Link>
     </nav>
+    </>
   )
 }
 
