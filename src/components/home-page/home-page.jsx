c632aef9ceef0bcf90575faf48fe0ec8e881f1d1
ijaz <ijazue6@gmail.com>
2023-01-20 16:32:18 +0500

    #commnet chagnes in home page

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -16,26 +16,26 @@ const Homepage = () => {
     }
   ];
 
-  const obj = {};
-  console.log(obj);
-  const urlShortener = (longURL = '') => {
-    let shortURL = "short.ly/" + longURL.replace(/[^a-z]/g, '').slice(-4);
-    if (!obj[shortURL]) {
-      obj[shortURL] = longURL;
-    };
-    return shortURL;
-  }
-  const urlRedirector = (shortURL = '') => {
-    return obj[shortURL];
-  };
-
-
 
   //dummy data
 
+  const urlArr = [];
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    const obj = {};
+    console.log(obj);
+    const urlShortener = (longURL = '') => {
+      let shortURL = "short.ly/" + longURL.replace(/[^a-z]/g, '').slice(-4);
+      if (!obj[shortURL]) {
+        obj[shortURL] = longURL;
+      };
+      return shortURL;
+    }
+    const urlRedirector = (shortURL = '') => {
+      return obj[shortURL];
+    };
 
 
     const short = urlShortener(url);
@@ -43,28 +43,31 @@ const Homepage = () => {
 
     console.log(short);
     // console.log(original);
-    
+
   }
 
   return (
     <>
-    <div className='container'>
-      <div className='box'>
-        <h1 >Paste the URL to be shortened</h1>
+      <div className='container'>
+        <div className='box'>
+          <h1 >Paste the URL to be shortened</h1>
           <Grid >
             <Grid>
-              <TextField
-                className='text-container'
-                placeholder='Enter Your URL'
-              />
-              <Button sx={{ marginLeft: 1 }} variant="contained" color="primary">
-                Generate</Button>
+              <form onSubmit={formSubmitHandler}>
+                <TextField
+                  value={url}
+                  onChange={(e) => (setUrl(e.target.value))}
+                  className='text-container'
+                  placeholder='Enter Your URL'
+                />
+                <button > Generate</button>
+              </form>
             </Grid>
 
           </Grid>
-          </div>
+        </div>
       </div>
-      
+
     </>
   )
 }
