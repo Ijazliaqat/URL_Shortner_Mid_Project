ea2327bd3a2fe750287c5f1b2a8678fbfa9c0450
ijaz <ijazue6@gmail.com>
2023-01-20 15:45:11 +0500

    #comment chagnes in home page

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -1,33 +1,76 @@
 import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import React from 'react'
+import { useState } from 'react';
+import { Form } from 'react-router-dom/dist';
 import './home-page.css'
 
 const Homepage = () => {
 
+  const [url, setUrl] = useState('');
+
+  const urlData = [
+    {
+      originalURL: "https://contoursoftware.com",
+      shortURL: "Short.ly://abc",
+      btn: <Button>Copy</Button>
+    }
+  ];
+
+  const obj = {};
+  console.log(obj);
+  const urlShortener = (longURL = '') => {
+    let shortURL = "short.ly/" + longURL.replace(/[^a-z]/g, '').slice(-4);
+    if (!obj[shortURL]) {
+      obj[shortURL] = longURL;
+    };
+    return shortURL;
+  }
+  const urlRedirector = (shortURL = '') => {
+    return obj[shortURL];
+  };
+
+
 
   //dummy data
-  const urlData = [{
-    originalURL: "https://contoursoftware.com",
-    shortURL: "Short.ly://abc",
-    btn: <Button>Copy</Button>
-  }];
+
+
+  const formSubmitHandler = (e) => {
+    e.preventDefault();
+
+
+    const short = urlShortener(url);
+    const original = urlRedirector(short);
+
+    console.log(short);
+    // console.log(original);
+    
+  }
 
   return (
     <>
       <Grid mt={30} className='home-container'>
         <Grid>
-          <TextField
-            className='text-container'
-            placeholder='Enter Your URL'
-          />
-          <Button sx={{ marginLeft: 3 }} variant="contained" color="success">
-            Generate</Button>
+          
+          <form onSubmit={formSubmitHandler}>
+
+            <TextField
+              value={url}
+              onChange={(e) => { setUrl(e.target.value) }}
+              className='text-container'
+              placeholder='Enter Your URL'
+            />
+        
+            <button type='submit' sx={{ marginLeft: 3 }} variant="contained" color="success">
+              Generate
+            </button>
+
+          </form>
         </Grid>
 
       </Grid>
       <Grid>
         <Grid className='table-data' mx={60} mt={5}>
-          <TableContainer  component={Paper}>
+          <TableContainer component={Paper}>
             <Table aria-label="simple table">
               <TableHead>
                 <TableRow>
