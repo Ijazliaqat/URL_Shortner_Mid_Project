d552a34c0659fdd3e2f4e0d91173304b345017da
ijaz <ijazue6@gmail.com>
2023-01-20 12:12:36 +0500

    #comment changes in home page

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -1,86 +1,60 @@
+import { Button, Grid, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import React from 'react'
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import {useCopyToClipboard} from "usehooks-ts";
-
-
-
+import './home-page.css'
 
 const Homepage = () => {
-  const [value,copy]=useCopyToClipboard()
-  const [date, setDate] = React.useState(null);
+
+
+  //dummy data
+  const urlData = [{
+    originalURL: "https://contoursoftware.com",
+    shortURL: "Short.ly://abc",
+    btn: <Button>Copy</Button>
+  }];
+
   return (
-  <>
+    <>
+      <Grid mt={30} className='home-container'>
+        <Grid>
+          <TextField
+            className='text-container'
+            placeholder='Enter Your URL'
+          />
+          <Button sx={{ marginLeft: 3 }} variant="contained" color="success">
+            Generate</Button>
+        </Grid>
 
-  {/* URL Input Generate Box */}
-    <div className='container'>
-      <h1>Paste your <span> URL </span> here </h1>
-        <div className='inputBox'>
-          &nbsp; &nbsp; <input type="text" placeholder='Paste your link here' />
-        <Button  variant="contained" color="success">Shorten</Button>
-    </div>
-
-  {/* URL Output Copy Box */}
-    <h1>Copy your <span> URL </span> from here </h1>
-    <div className='inputBox'>
-      &nbsp; &nbsp; <input type="text" placeholder='Paste your link here' />
-      <Button  onClick={()=> {copy('text')}} variant="contained" color="error">Copy</Button>
-    </div>
-
-  {/* Set Expiry Code Block */}
-    <h1>Set expiry for your <span> URL </span> from here </h1>
-    <div className='inputBox'>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DatePicker label="" value={value}onChange={(newValue) => {
-          setDate(newValue);
-        }}
-        renderInput={(params) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
-    </div>
-  </div>
-  {/* Background Animation #Ignore */}
-      <div>
-      <div className="background">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-    
-  </div>
-  </>
-      
-
-
-//     {/* <TextField
-//   id="outlined-name"
-//   full
-//   label="Name"
-//   //value={name}
-//   //onChange={handleChange}
-// /> */}
-
-
+      </Grid>
+      <Grid>
+        <Grid className='table-data' mx={60} mt={5}>
+          <TableContainer  component={Paper}>
+            <Table aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>No.</TableCell>
+                  <TableCell align="right">Original URL</TableCell>
+                  <TableCell align="right">Short URL</TableCell>
+                  <TableCell align="right">Copy</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {urlData.map((data, index) => (
+                  <TableRow
+                    key={index}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell align="right">{index + 1}</TableCell>
+                    <TableCell align="right">{data.originalURL}</TableCell>
+                    <TableCell align="right">{data.shortURL}</TableCell>
+                    <TableCell align="right">{data.btn}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      </Grid>
+    </>
   )
 }
 
