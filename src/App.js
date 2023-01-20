a8311651058d554d54d88a7a62961805aa6de133
aneeqgulzar <aneeqgulzaeonline@gmail.com>
2023-01-20 23:19:07 +0500

    footer-added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header/header";
 import History from "./components/history/history";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Footer from ".components/footer/footer";
 
 function App() {
   return (
